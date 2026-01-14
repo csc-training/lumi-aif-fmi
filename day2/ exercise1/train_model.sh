2d3ff02e4509a5cbcb393863722647a363ee7607
Iidahak <75577529+Iidahak@users.noreply.github.com>
2026-01-14 13:19:53 +0200

    Create train_model.sh
    Add batch script

---
+++ day2/ exercise1/train_model.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --account=project_xxxxxxxxx # Choose the project to be billed. Change to own project, if used outside of the course.
+#SBATCH --partition=gpu             # Which queue to use. Defines maximum time, memory, tasks, nodes and local storage for job.             
+#SBATCH --ntasks=1                  # Number of tasks. Upper limit depends on partition.
+#SBATCH --cpus-per-task=7           # How many processors work on one task. Upper limit depends on number of CPUs per GPU. In LUMI there are 7 CPU cores per one GPU.
+#SBATCH --time=03:00:00             # Maximum duration of the job. Upper limit depends on partition.
+#SBATCH --mem=40G                   # Reserved memory.
+#SBATCH --gpus=1                    # Number of GPUs 
+
+# Load Pytorch module
+module load pytorch/2.7
+
+# Activate virtual environment containing special packages
+source /projappl/project_xxxxxxxxx/fmi_course/bin/activate
+
+# Run the Python code
+srun python3 train_model.py
