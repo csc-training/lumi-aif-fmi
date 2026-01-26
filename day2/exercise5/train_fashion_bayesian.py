af50ad893950a5fc10cf1c173e0a4dcd4d4dd69a
Oskar Taubert <oskar.taubert@csc.fi>
2026-01-26 14:01:13 +0200

    added run script and minor fixes for exercise 5

--- day2/exercise5/train_fashion_bayesian.py
+++ day2/exercise5/train_fashion_bayesian.py
@@ -1,19 +1,21 @@
+import os
 import torch
 from torch import nn
 from torch.utils.data import DataLoader
-from torcvision import datasets
+from torchvision import datasets
 from torchvision.transforms import ToTensor
 from torch_blue import vi
 from tqdm import tqdm
 
 
-data_root = "/scratch/project_2017263/data/"
+data_root = f"/scratch/{os.environ['SLURM_JOB_ACCOUNT']}/data/"
+
 
 class Model(vi.VIModule):
     def __init__(self):
         super().__init__()
         self.f = nn.Sequential(
-            nn.flatten()
+            nn.Flatten(),
             vi.VILinear(28*28, 512),
             nn.ReLU(),
             vi.VILinear(512, 512),
@@ -23,7 +25,8 @@ class Model(vi.VIModule):
     def forward(self, x):
         return self.f(x)
 
-def train(dl, model, loss_fn, optimizer):
+def train(dl, model, loss_fn, optimizer, device=torch.device("cuda:0"), num_samples=10):
+    model = model.to(device)
     model.train()
     for step, batch in (enumerate(pbar:=tqdm(dl))):
         x, y = batch
@@ -31,13 +34,14 @@ def train(dl, model, loss_fn, optimizer):
         y = y.to(device)
 
         optimizer.zero_grad()
-        pred = model(x)
+        pred = model(x, samples=num_samples)
         loss = loss_fn(pred, y)
-        pbar.set_description(f"Loss: {loss.item:>7f}")
+        pbar.set_description(f"Loss: {(loss.item()):>7f}")
         loss.backward()
         optimizer.step()
 
-def test(dl, model, loss_fn):
+def test(dl, model, loss_fn, predictive_distribution, device=torch.device("cuda:0"), num_samples=10):
+    model = model.to(device)
     model.eval()
     loss, correct = 0.0, 0.0
     with torch.no_grad():
@@ -45,26 +49,26 @@ def test(dl, model, loss_fn):
             x, y = batch
             x = x.to(device)
             y = y.to(device)
-            samples = model(x)
+            samples = model(x, samples=num_samples)
             loss += loss_fn(samples, y).item()
             pred = predictive_distribution.predictive_parameters_from_samples(samples)
             correct += (pred.argmax(1) == y).type(torch.float).sum().item()
     loss /= len(dl)
     correct /= (len(dl.dataset))
     print(f"Test accuracy: {(100 * correct):>0.1f}%, Avg loss: {loss:>8f}")
+    # print(f"output shape: {samples.size()}")
 
 def main():
     epochs = 5
     learning_rate = 1e-4
     batch_size = 64
-    device = torch.device("cuda:0")
     train_ds = datasets.FashionMNIST(
         root=data_root,
         train=True,
         download=True,
         transform=ToTensor(),
     )
-    test_data = datasets.FashionMNIST(
+    test_ds = datasets.FashionMNIST(
         root=data_root,
         train=False,
         download=True,
@@ -74,20 +78,24 @@ def main():
 
     train_dl = DataLoader(train_ds, batch_size=batch_size)
     test_dl = DataLoader(test_ds, batch_size=batch_size)
-    model = Model().to(device)
+
+    model = Model()
+
     # NOTE return log probs of weights needed by KL loss
     model.return_log_probs = True
     print(model)
 
     predictive_distribution = vi.distributions.Categorical()
-    loss_fn = vi.KullbackLeiblerLoss(predictive_distribution, dataset_size=len(ds))
+    loss_fn = vi.KullbackLeiblerLoss(predictive_distribution, dataset_size=len(train_ds))
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
-    train(train_dl, model, loss_fn, optimizer)
 
     for epoch in range(epochs):
-        train()
-        print(loss/(torch.sum(y)/batch_size))
+        print("Epoch {epoch}:")
+        train(train_dl, model, loss_fn, optimizer)
+        test(train_dl, model, loss_fn, predictive_distribution)
+
+    # NOTE save checkpoint
 
 if __name__ == "__main__":
     main()
