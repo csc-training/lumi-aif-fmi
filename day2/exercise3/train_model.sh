6f3f7c5f70e54ad5ddf18ec8d33b10f0b0541641
Iidahak <75577529+Iidahak@users.noreply.github.com>
2026-01-26 12:28:48 +0200

    fix number of cpus per task in train_model.sh

--- day2/exercise3/train_model.sh
+++ day2/exercise3/train_model.sh
@@ -2,7 +2,7 @@
 #SBATCH --account=project_2017263   # Choose the project to be billed. Change to own project, if used outside of the course.
 #SBATCH --partition=gpusmall        # Which queue to use. Defines maximum time, memory, tasks, nodes and local storage for job.             
 #SBATCH --ntasks=1                  # Number of tasks. Upper limit depends on partition.
-#SBATCH --cpus-per-task=32           # How many processors work on one task. Upper limit depends on number of CPUs per GPU. In Mahti there are 32 CPU cores per one GPU.
+#SBATCH --cpus-per-task=8           # How many processors work on one task. Upper limit depends on number of CPUs per GPU. 
 #SBATCH --time=03:00:00             # Maximum duration of the job. Upper limit depends on partition.
 #SBATCH --mem=40G                   # Reserved memory.
 #SBATCH --gres=gpu:a100:1           # Number of GPUs 
